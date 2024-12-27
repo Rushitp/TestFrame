5e44cf81202c32dbd25287740228e223d107b9fc
rushit <sudanirushit67@gmail.com>
2024-12-27 21:52:36 +0530

    Initial Commit

---
+++ TestFrameTests/TestFrameTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestFrameTests.swift
+//  TestFrameTests
+//
+//  Created by Rushit Sudani on 27/12/24.
+//
+
+import Testing
+@testable import TestFrame
+
+struct TestFrameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
