ca0d7abf9b0d2769a29f9d63d29fbe1d467c799a
rushit <sudanirushit67@gmail.com>
2024-12-29 11:28:34 +0530

    First Commit

---
+++ TestFrame/managibg.swift
@@ -0,0 +1,19 @@
+//
+//  managibg.swift
+//  TestFrame
+//
+//  Created by Rushit Sudani on 27/12/24.
+//
+
+import Foundation
+import UIKit
+
+public class managetext {
+    
+    public init() {}
+    
+    public func test(message: String) {
+        print("\(message) Hello World")
+    }
+    
+}
